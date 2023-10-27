6021969049d18b93f23bef0e8f333ec400c710f1
Hamim <hamim99bd@gmail.com>
2023-10-27 12:11:15 +0600

    route,layout setup done

---
+++ src/Pages/Signup/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Signup = () => {
+  return (
+    <div>
+      <h1>This is Signup</h1>
+      
+    </div>
+  );
+};
+
+export default Signup;
---
+++ src/Routes/Routes.js
@@ -0,0 +1,17 @@
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home";
+
+const { createBrowserRouter } = require("react-router-dom");
+const { default: Signup } = require("../Pages/Signup/Signup");
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      { path: '/', element: <Home></Home> },
+      {path:'/signup',element:<Signup></Signup>}
+
+    ]
+  }
+])
