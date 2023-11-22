b3f413644b07e0ae16bf193b1d239873e9e85c7f
AkhlakPro <77771011+AkhlakPro@users.noreply.github.com>
2023-11-23 00:52:52 +0600

    s3 c1

--- backend/dbConnect.js
+++ backend/dbConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const MONGODB_URI = `mongodb+srv://mosqueDB:${process.env.DB_PASS}@cluster0.rdlfifr.mongodb.net/mosque-platform?retryWrites=true&w=majority`;
+const MONGODB_URI = `mongodb://mosqueDB:${process.env.DB_PASS}@ac-vyo8d9d-shard-00-00.rdlfifr.mongodb.net:27017,ac-vyo8d9d-shard-00-01.rdlfifr.mongodb.net:27017,ac-vyo8d9d-shard-00-02.rdlfifr.mongodb.net:27017/?ssl=true&replicaSet=atlas-p9mkrx-shard-0&authSource=admin&retryWrites=true&w=majority`
 async function dbConnect() {
  
     mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
