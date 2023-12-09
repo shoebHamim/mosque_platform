c58359c47f01701509d0b47507b07450b0bceb44
AkhlakPro <77771011+AkhlakPro@users.noreply.github.com>
2023-12-09 21:17:23 +0600

    Improve layout and add styling for announcements
    Refactor SingleRegistered component layout for better readability and user experience. Introduce cssform.css for consistent styling of the announcement form. Remove unused Carousel3 component and adjust flex properties to align elements properly. Add placeholder for new announcements and a submit button with appropriate styling.

--- src/Pages/Registered/SingleRegistered.js
+++ src/Pages/Registered/SingleRegistered.js
@@ -5,6 +5,7 @@ import Carousel3 from '../../components/carousal3';
 import Carousel2 from '../../components/carousal2';
 import { UserState } from 'realm-web';
 import Announcement from  '../announcement/announcement';
+import '../Registered/cssform.css';
 function SingleRegistered() {
   const [mosque, setMosque] = useState(null);
   const { id } = useParams();
@@ -29,44 +30,70 @@ function SingleRegistered() {
 
   return (
     <div className="bg-white text-black">
-      <div style={{ display: 'flex' }}>
+      <div style={{ display: "flex" }}>
         {/* Left Column */}
-<div style={{ flex: 0, padding: '0rem', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginLeft: '0rem' }}>
+        <div
+          style={{
+        flex: 0.5,
+            padding: "6rem",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            marginLeft: "0rem",
+          }}
+        >
           {/* Carousel at the top */}
-          <Carousel3 />
-
+          {/* <Carousel3 /> */}
+  
           {/* Details info */}
-      <div style = {{ flex: 0, padding: '0rem', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginLeft: '0rem' }}>
-            <h1 style={{ fontWeight: 'bold' }}>{mosque.name}</h1>
-            <h7 style={{ fontSize: 'smaller' }}>{mosque.location}</h7>
-            <h6 style={{ fontSize: 'smaller' }}>Division: {mosque.division}</h6>
+          <div
+            style={{
+              flex: 0,
+              padding: "0rem",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              marginLeft: "0rem",
+            }}
+          >
+            <h1 style={{ fontWeight: "bold" }}>{mosque.name}</h1>
+            <h7 style={{ fontSize: "smaller" }}>{mosque.location}</h7>
+            <h6 style={{ fontSize: "smaller" }}>Division: {mosque.division}</h6>
             <p>{mosque.description}</p>
             <Link to={mosque.infoURL}>
               <p className="link link-primary">See more details</p>
             </Link>
           </div>
         </div>
-
+  
         {/* Right Column */}
-        <div style={{ flex: 0, padding: '0rem', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-        
-         
-            <Announcement/>
-          
-          
-          {/* <div className="form-control">
-            <textarea className="textarea textarea-bordered w-max h-15" placeholder="New announcement"></textarea>
+        <div
+          style={{
+            flex: 0,
+            padding: "0rem",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div className="form-control">
+            <textarea
+              className="textarea textarea-bordered w-max h-15"
+              placeholder="New announcement"
+            />
             <Link to={`/announcement/${mosque._id}`}>
-            <button className="btn-sm text-white bg-blue-500 rounded-2xl">
-              Add new annoucment
-            </button>
-          </Link>
-          </div> */}
-        
-          
+              <button className="btn-sm text-white bg-blue-500 rounded-2xl">
+                Submit
+              </button>
+            </Link>
+          </div>
+          <Announcement />
         </div>
       </div>
-
+  
       {/* Go to Edit Page Button */}
       <div className="text-center">
         <Link to={`/Edit/${mosque._id}`}>
